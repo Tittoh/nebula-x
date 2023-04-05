2ab1beed6e6f8620dcd668f27e0c1e5bd5da9084
Tittoh <tittohb@gmail.com>
2023-04-05 04:17:33 +0300

    ✨Add vpn landing page

---
+++ apps/vpn-landing/src/components/misc/ImageContainer.tsx
@@ -0,0 +1,7 @@
+const ImageContainer = ({ src, alt, className }) => {
+  return (
+    <img src={src} alt={alt} className={`${className} relative`}/>
+  );
+};
+
+export default ImageContainer;
---
+++ apps/vpn-landing/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Feature from "../components/Feature";
+import Pricing from "../components/Pricing";
+import Hero from "../components/Hero";
+import Layout from "../components/Layout/Layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+        <Hero />
+        <Feature />
+        <Pricing />
+      </Layout>
+    </>
+  );
+}
